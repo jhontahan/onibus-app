b9f6d536b5165fe81f8a9187537f5f0725c04654
jhonathan.cruz <jhonathan.cruz@sankhya.com.br>
2022-12-26 18:32:40 -0300

    first commit

---
+++ src/componentes/formularioCad/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style"
+
+function FormCad(props){
+    return(
+        <S.FormStyle>
+            <h1>teste</h1>
+        </S.FormStyle>
+    )
+}
+
+export default FormCad;
